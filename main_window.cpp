a1d52bf8c750c103d7a02cd5d34d592dd602c44a
Jared Bruni <jaredbruni@gmail.com>
2022-07-11 00:07:20 -0700

    controls

--- main_window.cpp
+++ main_window.cpp
@@ -1,6 +1,16 @@
 #include"main_window.hpp"
+#include<QLabel>
 
 AC_MainWindow::AC_MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("AcidCam.Lite");
     setFixedSize(800, 600);
+
+
+    QLabel *lbl = new QLabel(this);
+    lbl->setText("Playback: ");
+    lbl->setGeometry(25, 50, 75, 20);
+    list_filename = new QLineEdit(this);
+    list_filename->setGeometry(100, 50, 100, 20);
+
+
 }
--- main_window.hpp
+++ main_window.hpp
@@ -3,11 +3,17 @@
 
 #include<QMainWindow>
 #include<QWidget>
+#include<QLineEdit>
+#include<QPushButton>
 
 class AC_MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     AC_MainWindow(QWidget *parent = 0);
+
+private:
+    QLineEdit *list_filename;
+    QPushButton *list_open;
 };
 
 #endif
